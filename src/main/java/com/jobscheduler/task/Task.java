c479111d5a36080a85ea7950cec3f0a1d62abaa2
Jonatan <jonatanav255@hotmail.com>
2025-11-04 22:47:02 -0600

    adding task

---
+++ src/main/java/com/jobscheduler/task/Task.java
@@ -0,0 +1,19 @@
+package com.jobscheduler.task;
+
+/**
+ * Represents a unit of work that can be executed by the scheduler.
+ *
+ * @param <T> the type of result this task produces
+ */
+@FunctionalInterface
+public interface Task<T> {
+
+    /**
+     * Executes the task and returns a result.
+     * This method will be called by a worker thread when the task is scheduled.
+     *
+     * @return the result of the task execution
+     * @throws Exception if the task execution fails
+     */
+    T execute() throws Exception;
+}
